d84a7fb905a84e5136266358172c51efc7d78f38
codepologist <ombu.oneal@yahoo.com>
2025-02-06 15:07:45 +0100

    UPDATED Repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,54 @@
 import { useState } from "react";
 import Search from "./components/Search";
+import irokotv from "../src/assets/images/irokotv_logo.svg";
+import search from "../src/assets/images/search.svg";
 
 const App = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
 
   return (
-    <main className="section is-fullheight is-flex is-justify-content-center is-align-items-center overflow-hidden">
+    <>
       <header>
+        <div className="container px-6 py-4">
+          <nav
+            className="navbar is-clipped is-transparent"
+            aria-label="main navigation"
+          >
+            <div className="navbar-brand px-4">
+              <a className="navbar-item">
+                <img src={irokotv} width="112" height="28" alt="IrokoTV" />
+              </a>
+              <a
+                role="button"
+                className="navbar-burger"
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+              >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+            </div>
+
+            <div className="navbar-menu px-4">
+              <div className="navbar-end">
+                <a className="navbar-item">About</a>
+                <a className="navbar-item">Contact</a>
+                <a className="navbar-item">
+                  <img src={search} />
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+      <main className="section is-fullheight is-flex is-justify-content-center is-align-items-center overflow-hidden">
         <section className="hero">
           <div className="hero-body">
             <h1 className="title has-text-white has-text-weight-bold has-text-centered">
-              Find <span className="text-gradient">Nollywood Movies</span>{" "}
+              Find <span className="text-gradient">Nollywood Movies</span>
               You&apos;ll Love
               <br />
               Without the Hassle
@@ -24,8 +62,8 @@ const App = () => {
             </h1>
           </div>
         </section>
-      </header>
-    </main>
+      </main>
+    </>
   );
 };
 
---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import irokotv from "./irokotv_logo.svg";
+import search from "./search.svg";
+import star from "./star.svg";
+
+export { irokotv, search, star };
